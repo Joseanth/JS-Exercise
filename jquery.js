d5f89cb7ac9090c29aeeb04cd578a92b210ff157
brian <bignatue@gmail.com4>
2019-06-27 21:50:39 -0400

    jquery file added

---
+++ jquery.js
@@ -0,0 +1,22 @@
+/** BOTH SCRIPT 1 and SCRIPT has the same result!  */
+// SCRIPT 1
+$.getJSON("data.json", function(data) {
+  $.each(data, function(key, value) {
+    $("#showData").append("<tr><td>"+value.Orders.orderId+"</td><td>"+value.Orders.Sales.SalesName+"</td><td>"+value.Orders.Sales.orderdate+"</td><td>"+value.Orders.Sales.Products.id+"</td><td>"+value.Orders.Sales.Products.id+"</td><td>"+value.Orders.Sales.Products.productname+"</td><td>"+value.Orders.Sales.Products.Price+"</td><td>"+value.Orders.Sales.Customer.companyname+"</td><td>"+value.Orders.Sales.Customer.contactname+"</td></tr>"); 
+  })
+})
+
+/** SCRIPT 2 */
+// $.ajax({
+//   url: 'json-file.json',
+//   dataType: 'json',
+//   type: 'get',
+//   cache: false,
+//   success: function(data) {
+//      $(data.Sales).each(function(index, value) { 
+//       $("#showData").append("<tr><td>"+value.Orders.orderId+"</td><td>"+value.Orders.Sales.SalesName+"</td><td>"+value.Orders.Sales.orderdate+"</td><td>"+value.Orders.Sales.Products.id+"</td><td>"+value.Orders.Sales.Products.id+"</td><td>"+value.Orders.Sales.Products.productname+"</td><td>"+value.Orders.Sales.Products.Price+"</td><td>"+value.Orders.Sales.Customer.companyname+"</td><td>"+value.Orders.Sales.Customer.contactname+"</td></tr>"); 
+//      });
+//   }
+// });
+ 
+ 
